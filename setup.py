87b9eec3f7079af5ff3d0096697ab7113d482027
MarcusJones <jones.0bj3@gmail.com>
2019-01-20 12:04:51 +0100

    add Setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='kaggle_utils',
+    version='0.1dev',
+    packages=['kaggle_utils',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
